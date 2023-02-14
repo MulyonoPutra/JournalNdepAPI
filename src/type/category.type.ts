fdc3273921f26e285e821e0f68aaf03670afe97d
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-02-14 21:12:09 +0700

    Undo Prettier for more readable

--- src/type/category.type.ts
+++ src/type/category.type.ts
@@ -1,7 +1,10 @@
 import { TypedResponse } from '../utility/typed-controller';
 import { ResponseEntity, ResponseMessage } from '../interface/response-entity';
 import { Category } from '../interface/category';
+import { Pagination } from "../interface/pagination";
 
 export type CategoryResponseType = TypedResponse<
-	ResponseMessage | ResponseEntity<Category[]> | ResponseEntity<Category>
->;
+    ResponseMessage
+  | ResponseEntity<Category[]>
+  | ResponseEntity<Category>
+  | ResponseEntity<Pagination<Category>>>;
--- src/type/file.type.ts
+++ src/type/file.type.ts
@@ -2,7 +2,5 @@ import { TypedRequest, TypedResponse } from '../utility/typed-controller';
 import { ResponseEntity, ResponseMessage } from '../interface/response-entity';
 import { Images } from '../interface/images';
 
-export type FileResponseType = TypedResponse<
-	ResponseMessage | ResponseEntity<Images[]> | ResponseEntity<Images>
->;
+export type FileResponseType = TypedResponse<ResponseMessage | ResponseEntity<Images[]> | ResponseEntity<Images>>;
 export type FileRequestType = TypedRequest<Record<string, never>, Images>;
