9360bd1b1725daab9a5eeae13b4034d68bafc702
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-02-15 17:57:10 +0700

    Update generate-token.ts

--- src/utility/generate-token.ts
+++ src/utility/generate-token.ts
@@ -1,13 +1,19 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import { LoginResponseType } from "../type/auth.type";
 
 const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
 export const generateAccessToken = (payload: object) => {
-	return jwt.sign(payload, `${ACCESS_TOKEN_SECRET}`, { expiresIn: '10m' });
+	return jwt.sign(payload, `${ACCESS_TOKEN_SECRET}`, { expiresIn: "30m" });
 };
 
-export const generateRefreshToken = (payload: object) => {
-	return jwt.sign(payload, `${REFRESH_TOKEN_SECRET}`, {
-		expiresIn: '1d',
+export const generateRefreshToken = (payload: object, res: LoginResponseType) => {
+	const refresh = jwt.sign(payload, `${REFRESH_TOKEN_SECRET}`, { expiresIn: "30d" });
+
+	res.cookie("refreshToken", refresh, {
+		httpOnly: true,
+		maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
 	});
+
+	return refresh;
 };
