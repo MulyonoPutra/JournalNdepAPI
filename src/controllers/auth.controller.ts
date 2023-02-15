c34c79b8fa5c75c0eb8151b0b458acee9c548779
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-02-15 17:56:52 +0700

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -23,9 +23,7 @@ import {
 } from '../type/auth.type';
 import { UserDTO } from '../interface/login';
 
-export const register =
-	(role?: string) =>
-	async (req: RegisterRequestType, res: RegisterResponseType) => {
+export const register = (role?: string) => async (req: RegisterRequestType, res: RegisterResponseType) => {
 		try {
 			const { username, account, password } = req.body;
 			const users = await UserSchema.findOne({ account });
@@ -63,10 +61,17 @@ export const login = async (req: LoginRequestType, res: LoginResponseType) => {
 	try {
 		const { account, password } = req.body;
 		const user = await UserSchema.findOne({ account });
+
 		if (!user) {
 			return res.status(400).json({
-				message: 'User not found!',
+				message: 'Invalid credentials!',
 			});
+		}
+
+		if (!await bcrypt.compare(password, user.password)) {
+			return res.status(400).send({
+				message: 'Invalid credentials'
+			})
 		}
 
 		await loginSuccessful(user, password, res);
@@ -75,20 +80,10 @@ export const login = async (req: LoginRequestType, res: LoginResponseType) => {
 	}
 };
 
-const loginSuccessful = async (
-	user: IUser,
-	passwords: string,
-	res: LoginResponseType
-) => {
-	const isValid = await bcrypt.compare(passwords, user.password);
-
-	// Check if password is valid
-	if (!isValid) {
-		return res.status(500).json({ message: 'Password is incorrect!' });
-	}
+const loginSuccessful = async (user: IUser, passwords: string, res: LoginResponseType) => {
 
 	const accessToken = generateAccessToken({ id: user._id });
-	const refreshToken = generateRefreshToken({ id: user._id });
+	const refreshToken = generateRefreshToken({ id: user._id }, res);
 
 	await UserSchema.findOneAndUpdate({ _id: user._id }, { refreshToken });
 
@@ -97,18 +92,8 @@ const loginSuccessful = async (
 		maxAge: 24 * 60 * 60 * 1000,
 	});
 
-	const {
-		_id,
-		images,
-		username,
-		account,
-		role,
-		phone,
-		dob,
-		description,
-		avatar,
-		address,
-	} = user;
+
+	const { _id, images, username, account, role, phone, dob, description, avatar, address } = user;
 
 	const userDTO: UserDTO = {
 		_id,
@@ -129,49 +114,34 @@ const loginSuccessful = async (
 	});
 };
 
-export const refreshToken = async (
-	req: RefreshTokenRequestType,
-	res: RefreshTokenResponseType
-) => {
+export const refreshToken = async (req: RefreshTokenRequestType, res: RefreshTokenResponseType) => {
 	try {
 		const token = req.cookies?.refreshToken;
 		if (!token) {
-			return res.status(200).json({
-				message: 'Refresh token not found! Please login again.',
-			});
+			return res.status(401).json({ message: 'Refresh token not found! Please login again.' });
 		}
 
-		const decoded = jwt.verify(
-			token,
-			`${Environment.refreshTokenSecret}`
-		) as IDecoded;
+		const decoded = jwt.verify(token, `${Environment.refreshTokenSecret}`) as IDecoded;
 		if (!decoded.id) {
-			return res.status(400).json({
-				message: 'Refresh token is invalid!',
-			});
+			return res.status(401).json({ message: 'Refresh token is invalid!' });
 		}
 
-		const user = await UserSchema.findOne({ _id: decoded.id }).select(
-			'+password'
-		);
+		const user = await UserSchema.findOne({ _id: decoded.id }).select('+password');
 		if (!user) {
 			return res.status(400).json({
 				message: 'This account does not exist!',
 			});
 		}
 
-		const newAccessToken = generateAccessToken({ id: user._id });
+		const accessToken = generateAccessToken({ id: user._id });
 
-		return res.status(200).json({ accessToken: newAccessToken });
+		return res.status(200).json({ accessToken });
 	} catch (e) {
 		console.log(e);
 	}
 };
 
-export const logout = async (
-	req: LogoutRequestType,
-	res: NewAccessTokenResponseType
-) => {
+export const logout = async (req: LogoutRequestType, res: NewAccessTokenResponseType) => {
 	try {
 		const token = req.cookies?.refreshToken;
 		if (!token) {
@@ -186,10 +156,7 @@ export const logout = async (
 		}
 
 		const userId = user[0]?._id;
-		await UserSchema.findOneAndUpdate(
-			{ _id: userId },
-			{ refreshToken: null }
-		);
+		await UserSchema.findOneAndUpdate({ _id: userId }, { refreshToken: '' });
 		res.clearCookie('refreshToken');
 		return res.status(200).json({ message: 'Successfully logout!' });
 	} catch (e) {
